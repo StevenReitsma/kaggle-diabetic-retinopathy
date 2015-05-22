86bc34db25afbea656ed1f310ea9704bfdc3232a
Steven Reitsma <steven@properchaos.nl>
2015-05-22 15:47:00 +0200

    Changed preprocessing to include equalization of histogram.

--- process.sh
+++ process.sh
@@ -1,1 +1,1 @@
-mogrify -path ./data/processed/train -fuzz 15% -trim -gravity center -resize 256x256 -extent 256x256 -background black ./data/train/*.jpeg
+mogrify -path ./data/processed/test -fuzz 10% -trim -gravity center -resize 256x256 -extent 256x256 -background black -equalize -quality 100 ./data/test/*.jpeg

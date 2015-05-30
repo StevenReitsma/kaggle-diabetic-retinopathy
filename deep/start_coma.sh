4b6b1bb456f14d59b546a4b3a0a2bc1c0e966394
Steven Reitsma <steven@properchaos.nl>
2015-05-30 14:09:12 +0200

    Fixed bug in COMA processing.

--- deep/start_coma.sh
+++ deep/start_coma.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Reserve 1 GPU
-#SBATCH --gres gpu:1
+#SBATCH --gres=gpu:1
 
 # Set maximum run time to two days
 #SBATCH --time=2-00:00:00
 
 # Run script on long partition (lowest priority)
-#SBATCH --partition long
+#SBATCH --partition=long
 
 # Reserve 4GB memory per node
 #SBATCH --mem=4G
@@ -19,7 +19,7 @@
 #SBATCH --cpus-per-task=2
 
 # Set name
-#SBATCH -J DBR_0001
+#SBATCH --job-name=DBR_0001
 
 # Set notification email
 #SBATCH --mail-user=s.reitsma@ru.nl
@@ -32,6 +32,12 @@ SCRIPT_DIR=/home/sreitsma/coma/kaggle-diabetic-retinopathy
 # Create necessary directories
 mkdir -p $SCRATCH_DIR
 mkdir -p $SCRATCH_DIR/models
+
+# Export necessary environmental variables
+export LD_LIBRARY_PATH=/home/sreitsma/cudnn-6.5-linux-x64-v2:/usr/local/cuda-6.5/lib64:$LD_LIBRARY_PATH
+export PATH=/usr/local/cuda-6.5/bin:$PATH
+export LIBRARY_PATH=/home/sreitsma/cudnn-6.5-linux-x64-v2:/usr/local/cuda-6.5/lib64:$LIBRARY_PATH
+export CPATH=/home/sreitsma/cudnn-6.5-linux-x64-v2:/usr/local/cuda-6.5/lib64:$CPATH
 
 # Copy files from /vol/astro0 to /scratch over InfiniBand for local I/O access
 echo "Copying files from /vol/astro0 to /scratch"

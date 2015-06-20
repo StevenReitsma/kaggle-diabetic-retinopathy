13fbc7f877f3e371f5f3ca1f2425cf1fced051fa
Guido Zuidhof <me@guido.io>
2015-06-20 20:19:25 +0200

    Move preprocess files to folder

--- process.sh
+++
@@ -1,2 +0,0 @@
-mogrify -path ./data/processed_512/train -fuzz 10% -trim -gravity center -resize 512x512 -extent 512x512 -background black -equalize -quality 100 ./data/train/*.jpeg
-mogrify -path ./data/processed_512/test -fuzz 10% -trim -gravity center -resize 512x512 -extent 512x512 -background black -equalize -quality 100 ./data/test/*.jpeg

6917d5160f8a882dc8a8b33c7d3f560d407ff14a
Guido <me@guido.io>
2015-06-21 02:47:51 +0200

    Batch file interpreter strips single %s..

--- preprocess/train_256_meuk.bat
+++ preprocess/train_256_meuk.bat
@@ -1,1 +1,1 @@
-mogrify -path ./../data/train_256_meuk -fuzz 10% -trim -gravity center -resize 256x256 -extent 256x256 -background black -quality 100 ./../data/train/*.jpeg
+mogrify -path ./../data/train_256_meuk -fuzz 10%% -trim -gravity center -resize 256x256 -extent 256x256 -background black -quality 100 ./../data/train/*.jpeg
--- preprocess/train_magick.bat
+++ preprocess/train_magick.bat
@@ -1,1 +1,1 @@
-mogrify -path ./../data/train_magick -fuzz 10% -trim -gravity center -resize 256x256 -extent 256x256 -background black -equalize -quality 100 ./../data/train_circle_masked/*.jpeg
+mogrify -path ./../data/train_magick -fuzz 10%% -trim -gravity center -resize 256x256 -extent 256x256 -background black -equalize -quality 100 ./../data/train_circle_masked/*.jpeg
